8e6fed31b731ca3931708b179a5bb22fdf9ab979
Xalizar6 <dave.rukamp@gmail.com>
2025-03-29 02:39:08 -0500

    implement pumpkin planting and growth logic with retry mechanism

--- pumpkins.py
+++ pumpkins.py
@@ -1,21 +1,33 @@
 import utils
 clear()
 world_size = get_world_size()
+total_cells = world_size * world_size
+
+def plant_and_grow():
+    # Plants a pumpkin and ensures it grows fully
+    plant(Entities.Pumpkin)
+    while not can_harvest():
+        use_item(Items.Fertilizer)
+        if get_entity_type() == None:
+            plant(Entities.Pumpkin)
+    return True
 
 while True:
-    grown_pumpkins = 0
     quick_print('Reset pumpkin counter')
+    grown_pumpkins = 0
+
     for x in range(world_size):
         for i in range(world_size):
             if get_ground_type() != Grounds.Soil:
                 till()
             utils.water()
-            if get_entity_type() == None:
-                plant(Entities.Pumpkin)
-            if get_entity_type() == Entities.Pumpkin and can_harvest():
+
+            # Plant and grow pumpkin, retry if it dies
+            if plant_and_grow():
                 grown_pumpkins += 1
                 quick_print(grown_pumpkins)
-            if grown_pumpkins == 16:
-                if can_harvest():
-                    harvest()
+
+            if grown_pumpkins == total_cells:
+                use_item(Items.Weird_Substance)
+                harvest()
             utils.travel()
