78baeed91077081d0fb5a07c192fbeb571e66fb6
Xalizar6 <dave.rukamp@gmail.com>
2025-04-03 21:34:58 -0500

    refactor pumpkin farming script for improved structure and clarity

--- pumpkins.py
+++ pumpkins.py
@@ -1,7 +1,25 @@
-import utils
-clear()
-world_size = get_world_size()
-total_cells = world_size * world_size
+# Version 1.0.1
+# Script to plant and grow pumpkins across entire map grid
+
+def farm_pumpkins():
+    # quick_print('Reset pumpkin counter')
+    grown_pumpkins = 0
+
+    for x in range(utils.world_size):
+        for y in range(utils.world_size):
+            if get_ground_type() != Grounds.Soil:
+                till()
+            utils.water()
+
+            # Plant and grow pumpkin, retry if it dies
+            if plant_and_grow():
+                grown_pumpkins += 1
+                # quick_print(grown_pumpkins)
+
+            if grown_pumpkins == total_cells:
+                use_item(Items.Weird_Substance)
+                harvest()
+            utils.travel()
 
 def plant_and_grow():
     # Plants a pumpkin and ensures it grows fully
@@ -12,22 +30,11 @@ def plant_and_grow():
             plant(Entities.Pumpkin)
     return True
 
-while True:
-    quick_print('Reset pumpkin counter')
-    grown_pumpkins = 0
 
-    for x in range(world_size):
-        for i in range(world_size):
-            if get_ground_type() != Grounds.Soil:
-                till()
-            utils.water()
+import utils
+total_cells = utils.world_size * utils.world_size
 
-            # Plant and grow pumpkin, retry if it dies
-            if plant_and_grow():
-                grown_pumpkins += 1
-                quick_print(grown_pumpkins)
-
-            if grown_pumpkins == total_cells:
-                use_item(Items.Weird_Substance)
-                harvest()
-            utils.travel()
+if __name__ == "__main__":
+    clear()
+    while True:
+        farm_pumpkins()
